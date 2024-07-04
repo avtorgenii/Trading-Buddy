0237210da4ac68a988cc845a583a659825f8f51d
Aleksandr <103767386+avtorgenii@users.noreply.github.com>
2024-07-04 14:59:58 +0200

    Dealing with bugs

---
+++ main.py
@@ -0,0 +1,9 @@
+from os import getenv
+
+from app.server import app
+
+if __name__ == "__main__":
+    import uvicorn
+
+    port = int(getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
